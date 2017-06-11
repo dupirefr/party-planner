8c5be622278c1fc672afe9583299e4a822d4a6f4
dupirefr <dupire.francois+pro@gmail.com>
2017-06-11 10:33:04 +0200

    Dummy HelloWorld Controller

--- src/main/java/com/dupirefr/HomeController.java
+++ src/main/java/com/dupirefr/HomeController.java
@@ -1,6 +1,7 @@
 package com.dupirefr;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,9 +16,8 @@ public class HomeController {
 	 * Methods
 	 */
 	@RequestMapping("/")
-	@ResponseBody
-	public String home() {
-		return "Hello World!";
+	public String indexAction() {
+		return "home";
 	}
 
 }
