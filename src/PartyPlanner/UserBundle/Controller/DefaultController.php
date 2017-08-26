30694d0f03d1aa75d5039ec254f1ef6c3d2d0e1a
dupirefr <dupire.francois+pro@gmail.com>
2017-08-26 12:17:03 +0200

    Application d'un filtre anti-vomitif sur le design

--- src/PartyPlanner/UserBundle/Controller/DefaultController.php
+++ src/PartyPlanner/UserBundle/Controller/DefaultController.php
@@ -6,26 +6,42 @@ use PartyPlanner\UserBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      */
     public function indexAction()
     {
         return $this->render('UserBundle:Default:index.html.twig');
     }
 
+	/**
+	 * @Route("/signin", name="signin")
+	 *
+	 * @param Request $request
+	 * @return Response
+	 */
+    public function signInAction(Request $request)
+    {
+
+    }
+
     /**
-     * @Route("/signup")
+     * @Route("/signup", name="signup")
+     *
      * @param Request $request
+     * @return Response
      */
     public function signUpAction(Request $request)
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
+
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
             $user->setPassword(password_hash($user->getPassword(), PASSWORD_BCRYPT));
@@ -33,6 +49,7 @@ class DefaultController extends Controller
             $entityManager->persist($user);
             $entityManager->flush();
         }
+
         return $this->render('UserBundle:Default:signup.html.twig', array('form' => $form->createView()));
     }
 }
